87ad1cd242d0280310c07ddc86447faefe856587
kiran <software.salyani@gmail.com>
2021-05-22 18:03:01 +0545

    created model and jsonresponse view

---
+++ watchmate/watchlist_app/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+from watchlist_app.views import movie_list, movie_details
+
+urlpatterns = [
+    path('list/', movie_list, name='movie-list'),
+    path('list/<int:pk>', movie_details, name='movie-details'),
+
+]
