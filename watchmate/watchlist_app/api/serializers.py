39f9e8006f825d35c35b18f06b98851b414add15
kiran <software.salyani@gmail.com>
2021-05-23 20:24:41 +0545

    field level and object level validation implemented

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -16,3 +16,13 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active')
         instance.save()
         return instance
+
+    def validate_name(self, value):
+        if len(value)< 2:
+            raise serializers.ValidationError("Name is too short!")
+        return value
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and descriptions should be differents")
+        return data
