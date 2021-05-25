7f545b322df9ccec326c07f0d90dcb79f60c680b
salyani_software <software.salyani@gmail.com>
2021-05-25 19:52:54 +0545

    added validators and validation in serializer

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def name_length(value):
+    if len(value)< 2:
+        raise serializers.ValidationError("name must be greater than 2 characters")
+    return value
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -17,10 +22,10 @@ class MovieSerializer(serializers.Serializer):
         instance.save()
         return instance
 
-    def validate_name(self, value):
-        if len(value)< 2:
-            raise serializers.ValidationError("Name is too short!")
-        return value
+    # def validate_name(self, value):
+    #     if len(value)< 2:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     return value
 
     def validate(self, data):
         if data['name'] == data['description']:
