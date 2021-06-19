e46fe618a3bd1c08f4d3902cd0f47ab91723576b
salyani_software <software.salyani@gmail.com>
2021-06-19 21:08:12 +0545

    implemented generic api view all method and url review create for specific movie

--- watchmate/watchlist_app/api/serializers.py
+++ watchmate/watchlist_app/api/serializers.py
@@ -5,7 +5,8 @@ from watchlist_app.models import WatchList, StreamPlatform, Review
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model= Review
-        fields =  "__all__"
+        exclude = ('watchlist',)
+        # fields =  "__all__"
 
 class WatchListSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
