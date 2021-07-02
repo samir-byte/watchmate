b818c6d2e83a09815978a3ebb92f46ee2e7516a7
salyani_software <software.salyani@gmail.com>
2021-07-02 06:37:29 +0545

    changes made

---
+++ watchmate/user_app/api/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from watchlist_app.models import User
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
+    class Meta:
+        model = User
+        fields =  ['username', 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({'error': 'P1 and P2 must be same!'})
+        if User.objects.filter(email= self.validated_data['email']).exists():
+            raise serializers.ValidationError({'error':'Email already exists!'})
+
+        account = User(email = self.validated_data['email'], username = self.validated_data['username'])
+        account.set_password(password)
+        account.save()
+
+        return account
